fc31ef7e3b1719c552641851b1f97a13ca6c8c6a
Jan Nowakowski <nowakowski@arktis-detectors.com>
2020-11-16 19:52:43 +0100

    ADD: swapEverySecondNode

--- swap_every_second_node.cpp
+++ swap_every_second_node.cpp
@@ -5,19 +5,38 @@
 #include <iostream>
 #include "data_structures/LinkedList.h"
 
-void swapEverySecondNode(Node* head)
+Node* swapEverySecondNode(Node* head)
 {
     if (!head || !head->getNext())
-        return;
-    // TODO
-
+        return head;
+    Node* new_head = head->getNext();
+    Node* current = head;
+    Node *tail, *temp;
+    unsigned node_number = 0;
+    while (current) {
+        temp = current->getNext();
+        if (node_number % 2) {
+            current->setNext(tail);
+        } else {
+            tail = current;
+            if (!temp)
+                current->setNext(nullptr);
+            else if (current->getNext() && current->getNext()->getNext() && current->getNext()->getNext()->getNext())
+                current->setNext(current->getNext()->getNext()->getNext());
+            else
+                current->setNext(current->getNext()->getNext());
+        }
+        current = temp;
+        node_number++;
+    }
+    return new_head;
 }
 
 int main() {
-    std::vector<int> all_nodes = {1, 2, 3, 4};//{1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<int> all_nodes = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     Node* node = new Node(all_nodes);
     std::cout << node->getString() << std::endl;
-    swapEverySecondNode(node);
+    node = swapEverySecondNode(node);
     std::cout << node->getString() << std::endl;
     return 0;
 }
