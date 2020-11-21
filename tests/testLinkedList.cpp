0b3d07d8b7036a1fbda948c9ecb8320dc380f870
Jan Nowakowski <nowakowski@arktis-detectors.com>
2020-11-21 20:06:51 +0100

    FIX: Memory leaks

--- tests/testLinkedList.cpp
+++ tests/testLinkedList.cpp
@@ -8,38 +8,48 @@
 #include "data_structures/LinkedList.h"
 #include "algorithms/linked_list_algorithms.h"
 
-TEST(LinkedListTest, ConstructorTest) {
+TEST(LinkedListTest, ConstructorTest)
+{
     std::vector<int> all_nodes = {};
     auto* node = new Node<int>(all_nodes);
     EXPECT_TRUE(node->getValue() == 0);
     EXPECT_TRUE(node->getNext() == nullptr);
+    delete node;
 
     all_nodes = {1, 2, 3, 4, 5};
     node = new Node<int>(all_nodes);
+    Node<int>* current = node;
     for (int each : all_nodes) {
-        EXPECT_TRUE(each == node->getValue());
-        node = node->getNext();
+        EXPECT_TRUE(each == current->getValue());
+        current = current->getNext();
     }
-    EXPECT_TRUE(node == nullptr);
+    EXPECT_TRUE(current == nullptr);
+    delete node;
 }
 
-TEST(LinkedListTest, GetStringTest) {
+TEST(LinkedListTest, GetStringTest)
+{
     std::vector<int> all_nodes = {};
     auto* node = new Node<int>(all_nodes);
     EXPECT_EQ(node->getString(), "0");
+    delete node;
+
     all_nodes = {1, 2, 3, 4, 5};
     node = new Node<int>(all_nodes);
     EXPECT_EQ(node->getString(), "1 -> 2 -> 3 -> 4 -> 5");
+    delete node;
 }
 
 
-TEST(LinkedListTest, SwapEverySecondTest) {
+TEST(LinkedListTest, SwapEverySecondTest)
+{
     std::vector<int> all_nodes = {};
     auto* node = new Node<int>(all_nodes);
     node = swapEverySecondNode(node);
     EXPECT_EQ(node->getString(), "0");
     swapEverySecondNodeByValue(node);
     EXPECT_EQ(node->getString(), "0");
+    delete node;
 
     all_nodes = {1, 2, 3, 4};
     node = new Node<int>(all_nodes);
@@ -49,6 +59,7 @@ TEST(LinkedListTest, SwapEverySecondTest) {
     EXPECT_EQ(node->getString(), "1 -> 2 -> 3 -> 4");
     node = swapEverySecondNode(node);
     EXPECT_EQ(node->getString(), "2 -> 1 -> 4 -> 3");
+    delete node;
 
     std::vector<double> all_nodes_double = {1.2, 2.3, 3.5, 4.6, 5.9};
     auto* node_double = new Node<double>(all_nodes_double);
@@ -58,28 +69,38 @@ TEST(LinkedListTest, SwapEverySecondTest) {
     EXPECT_EQ(node_double->getString(), "1.200000 -> 2.300000 -> 3.500000 -> 4.600000 -> 5.900000");
     node_double = swapEverySecondNode(node_double);
     EXPECT_EQ(node_double->getString(), "2.300000 -> 1.200000 -> 4.600000 -> 3.500000 -> 5.900000");
+    delete node_double;
 }
 
 
-TEST(LinkedList, HasLoop) {
+TEST(LinkedList, HasLoop)
+{
     auto* node = new Node<int>();
-
     EXPECT_FALSE(hasLoop(node));
-    std::vector<int> all_nodes = {};
+    delete node;
+
+    std::vector<int> all_nodes = {1, 2, 3};
     node = new Node<int>(all_nodes);
     EXPECT_FALSE(hasLoop(node));
     all_nodes = {1, 2, 3};
+    delete node;
+
     node = new Node<int>(all_nodes);
     EXPECT_FALSE(hasLoop(node));
     node->getNext()->getNext()->setNext(node);
     EXPECT_TRUE(hasLoop(node));
+    node->getNext()->getNext()->setNext(nullptr);
+    delete node;
+
     node = new Node<int>(all_nodes);
     node->getNext()->getNext()->setNext(node->getNext());
     EXPECT_TRUE(hasLoop(node));
-
+    node->getNext()->getNext()->setNext(nullptr);
+    delete node;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     std::cout << "------------------------------ Starting LinkedList tests -----------------------------" << std::endl;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
